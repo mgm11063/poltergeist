9f69cfdcd03b4f30135dbdcf1887a132126be447
문경민 <mgm11063@gmail.com>
2023-09-16 12:49:00 +0900

    [setup] React / Style Package / Router intall

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@chakra-ui/react';
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <Text fontSize={"3xl"} color={"red"}>hi</Text>
+    </div>
+  )
+}
+
+export default App;
